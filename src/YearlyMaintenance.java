7fe2b99a59f89038a30d46682e8bb1205672167b
Andrew <stewa2a8@mail.uc.edu>
2022-09-18 22:47:05 -0400

    completed yearly maintenance

---
+++ src/YearlyMaintenance.java
@@ -0,0 +1,12 @@
+public class YearlyMaintenance {
+    public YearlyMaintenance() {
+    }
+    public static void main(String[] args) {
+        double springCost = 100.0;
+        double summerCost = 75.55;
+        double fallCost = 105.43;
+        double winterCost = 99.99;
+        double totalYearlyMaintenanceCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The total yearly maintenance cost is: " + totalYearlyMaintenanceCost);
+    }
+}
